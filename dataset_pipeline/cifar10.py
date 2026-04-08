5e379b75643f57e74f865173fd1ca576b0aa70ec
yug pavasiya <yugpavasiya@outlook.com>
2026-04-08 13:00:55 +0530

    ADDED TRansforms

--- dataset_pipeline/cifar10.py
+++ dataset_pipeline/cifar10.py
@@ -1,19 +1,32 @@
-import torchvision.transforms.v2 as transforms
+from torchvision.transforms import v2
 from torchvision.datasets import CIFAR10
 import torch
 
 
 def get_datasets():
 
-    transform = transforms.Compose(
+    train_transform = v2.Compose(
         [
-            transforms.Resize((32, 32)),
-            transforms.ToImage(),
-            transforms.ToDtype(torch.float32, scale=True),
+            v2.RandomCrop(32, padding=4),
+            v2.RandomHorizontalFlip(),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ]
     )
-    train_dataset = CIFAR10(root="./data", train=True, download=True, transform=transform)
+    test_transform = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ]
+    )
+    train_dataset = CIFAR10(
+        root="./data", train=True, download=True, transform=train_transform
+    )
 
-    test_dataset = CIFAR10(root="./data", train=False, download=True, transform=transform)
+    test_dataset = CIFAR10(
+        root="./data", train=False, download=True, transform=test_transform
+    )
 
     return train_dataset, test_dataset
