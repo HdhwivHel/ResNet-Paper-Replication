8489505c157d1d3f81e8b1fbbb46a2aeb06c9841
yug pavasiya <yugpavasiya@outlook.com>
2026-04-08 11:44:14 +0530

    architecture changed;ignored cache files

--- models/resnet.py
+++ models/resnet.py
@@ -3,13 +3,22 @@ import torch.nn as nn
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        downsample: nn.Module = None,
+    ):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=stride, padding=1
+        )
         self.batchnorm1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
         self.batchnorm2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
 
     def forward(self, x):
         inp = x
@@ -18,10 +27,62 @@ class BasicBlock(nn.Module):
         x = self.relu(x)
         x = self.conv2(x)
         x = self.batchnorm2(x)
+        if self.downsample is not None:
+            inp = self.downsample(inp)
         x = x + inp  # skip connection implementation
         x = self.relu(x)
         return x
 
 
 class ResNet18(nn.Module):
-    
+    def __init__(self, num_classes: int):
+        super(ResNet18, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.batchnorm1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Define the layers of ResNet-18
+        self.layer1 = self._make_layer(64, 64, 2)
+        self.layer2 = self._make_layer(64, 128, 2, stride=2)
+        self.layer3 = self._make_layer(128, 256, 2, stride=2)
+        self.layer4 = self._make_layer(256, 512, 2, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(
+        self, in_channels: int, out_channels: int, num_blocks: int, stride: int = 1
+    ):
+        layers = []
+        downsample = None
+        if stride != 1 or in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        layers.append(BasicBlock(in_channels, out_channels, stride, downsample))
+        in_channels = out_channels
+
+        for _ in range(1, num_blocks):
+            layers.append(BasicBlock(in_channels, out_channels))
+            in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = self.flatten(x)
+        x = self.fc(x)
+        return x
